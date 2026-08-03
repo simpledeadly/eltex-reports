c06e58b14ba06b9d43cf1444c67352a458b3a589
simpledeadly <markmarkov94186@gmail.com>
2026-08-03 17:33:19 +0700

    feat(m3t1): add cmdl-line args parsing

--- module3/01/main.c
+++ module3/01/main.c
@@ -1,6 +1,34 @@
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main() {
-  printf("Hello, module 3 task 1\n");
+int main(int argc, char *argv[]) {
+  char *fifo_name = NULL;
+  int opt;
+
+  while ((opt = getopt(argc, argv, "p:")) != -1) {
+    if (opt == 'p') {
+      fifo_name = optarg;
+    } else {
+      fprintf(stderr,
+              "Using: %s [-p fifo_name] file1 file2 ...\nTwo files at least\n",
+              argv[0]);
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  if (optind >= argc) {
+    fprintf(stderr, "Enter files to copy.\n");
+    fprintf(stderr,
+            "Using: %s [-p fifo_name] file1 file2 ...\nTwo files at least\n",
+            argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
   return 0;
 }
