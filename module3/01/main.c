c09fb57c79bf635dab790de6f34a14da85a6bfc7
simpledeadly <markmarkov94186@gmail.com>
2026-08-03 18:49:03 +0700

    feat(m3t1): configure child process IPC descriptors

--- module3/01/main.c
+++ module3/01/main.c
@@ -75,7 +75,33 @@ int main(int argc, char *argv[]) {
 
   if (pid == 0) {
     printf("[Child] Child process started\n");
-    exit(EXIT_SUCCESS);
+
+    int read_fd;
+    int write_fd;
+
+    if (fifo_name == NULL) {
+      // if pipe
+      close(fd_p2c[1]);
+      close(fd_c2p[0]);
+
+      read_fd = fd_p2c[0];
+      write_fd = fd_c2p[1];
+    } else {
+      // if fifo
+      int open_write = open(fifo_c2p_name, O_WRONLY); // O_WRONLY === 1, write
+      if (open_write == -1) {
+        perror("Child open write FIFO");
+        exit(EXIT_FAILURE);
+      }
+      write_fd = open_write;
+
+      int open_read = open(fifo_name, O_RDONLY); // O_RDONLY === 0, read
+      if (open_read == -1) {
+        perror("Child open read FIFO");
+        exit(EXIT_FAILURE);
+      }
+      read_fd = open_read;
+    }
   } else {
     printf("[Parent] Parent created child process with PID = %d\n", pid);
     waitpid(pid, NULL, 0);
