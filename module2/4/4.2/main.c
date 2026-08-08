a79d457efbe22281b77bf99d6e689a4123375919
simpledeadly <markmarkov94186@gmail.com>
2026-08-08 21:15:45 +0700

    feat(m2t4.2): add main logic, done task

--- module2/4/4.2/main.c
+++ module2/4/4.2/main.c
@@ -1,6 +1,28 @@
 #include "pqueue.h"
+#include <stdio.h>
 
 int main() {
-  enqueue("test", 100);
+  enqueue("msg1", 50);
+  enqueue("msg2", 200);
+  enqueue("msg3", 10);
+  enqueue("msg4", 200);
+
+  printf("Queue after enqueue:\n");
+  print_queue();
+
+  char buf[64];
+  uint8_t prio;
+
+  dequeue_first(buf, &prio);
+  printf("dequeue_first: %s (%u)\n", buf, prio);
+
+  dequeue_exact_priority(10, buf, &prio);
+  printf("dequeue_exact_priority(10): %s (%u)\n", buf, prio);
+
+  dequeue_ge_priority(100, buf, &prio);
+  printf("dequeue_ge_priority(100): %s (%u)\n", buf, prio);
+
+  print_queue();
+  clear_queue();
   return 0;
 }
