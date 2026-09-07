ce193789c80dbbebe7aa6f32a0daac1867688d87
simpledeadly <markmarkov94186@gmail.com>
2026-09-08 00:13:39 +0700

    feat(m3t6): improve

--- module3/06/main.c
+++ module3/06/main.c
@@ -46,33 +46,46 @@ int main(int argc, char *argv[]) {
   snprintf(username, sizeof(username), "%s", argv[1]);
 
   sock = socket(AF_INET, SOCK_DGRAM, 0);
-  if (sock == -1) {
+  if (sock < 0) {
     perror("socket");
     exit(EXIT_FAILURE);
   }
 
   int broadcast_enable = 1;
-  setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcast_enable,
-             sizeof(broadcast_enable));
+  if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcast_enable,
+                 sizeof(broadcast_enable)) < 0) {
+    perror("setsockopt SO_BROADCAST");
+    close(sock);
+    exit(EXIT_FAILURE);
+  }
 
   int reuse = 1;
-  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+  if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+    perror("setsockopt SO_REUSEADDR");
+    close(sock);
+    exit(EXIT_FAILURE);
+  }
 
   struct sockaddr_in my_addr;
   memset(&my_addr, 0, sizeof(my_addr));
   my_addr.sin_family = AF_INET;
   my_addr.sin_port = htons(PORT);
-  my_addr.sin_addr.s_addr = INADDR_ANY;
+  my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-  if (bind(sock, (struct sockaddr *)&my_addr, sizeof(my_addr)) == -1) {
+  if (bind(sock, (struct sockaddr *)&my_addr, sizeof(my_addr)) < 0) {
     perror("bind");
+    close(sock);
     exit(EXIT_FAILURE);
   }
 
   memset(&broadcast_addr, 0, sizeof(broadcast_addr));
   broadcast_addr.sin_family = AF_INET;
   broadcast_addr.sin_port = htons(PORT);
-  broadcast_addr.sin_addr.s_addr = inet_addr(BROADCAST_ADDR);
+  if (inet_pton(AF_INET, BROADCAST_ADDR, &broadcast_addr.sin_addr) <= 0) {
+    fprintf(stderr, "Invalid broadcast IP address: %s\n", BROADCAST_ADDR);
+    close(sock);
+    exit(EXIT_FAILURE);
+  }
 
   signal(SIGINT, on_sigint);
   signal(SIGHUP, on_sigint);
@@ -82,6 +95,7 @@ int main(int argc, char *argv[]) {
   pid_t pid = fork();
   if (pid == -1) {
     perror("fork");
+    close(sock);
     exit(EXIT_FAILURE);
   }
 
@@ -98,12 +112,12 @@ int main(int argc, char *argv[]) {
     }
   } else {
     char buf[BUF_SIZE];
-    struct sockaddr from_addr;
+    struct sockaddr_in from_addr;
     socklen_t from_len = sizeof(from_addr);
 
     while (1) {
-      ssize_t n =
-          recvfrom(sock, buf, sizeof(buf) - 1, 0, &from_addr, &from_len);
+      ssize_t n = recvfrom(sock, buf, sizeof(buf) - 1, 0,
+                           (struct sockaddr *)&from_addr, &from_len);
       if (n <= 0)
         continue;
       buf[n] = '\0';
@@ -112,10 +126,16 @@ int main(int argc, char *argv[]) {
           (buf[strlen(username)] == ':' || buf[strlen(username)] == ' '))
         continue;
 
-      printf("%s\n", buf);
+      char sender_ip[INET_ADDRSTRLEN];
+      inet_ntop(AF_INET, &from_addr.sin_addr, sender_ip, sizeof(sender_ip));
+      int sender_port = ntohs(from_addr.sin_port);
+
+      printf("[%s:%d] %s\n", sender_ip, sender_port, buf);
+
       fflush(stdout);
     }
   }
 
+  close(sock);
   return 0;
 }
